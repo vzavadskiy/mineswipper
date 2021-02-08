ab25ebcdfe662d1456b67d1b4601704ec20cde1d
Vitalik Zavadskiy <v.zavadskiy@dunice.net>
2021-02-08 17:39:56 +0300

    learn JS

---
+++ main.js
@@ -0,0 +1,25 @@
+// "use strict"
+
+// // // let age1 = null
+// //  let name = "Mery"
+// // // alert(age1);
+// // // alert(age2);
+
+// // function sayName(_name) {
+// //     alert(_name);
+// //   }
+
+// //   sayName(name);
+
+
+// //   let age = prompt('Сколько тебе лет?', 100);
+
+// // alert(`Тебе ${age} лет!`); // Тебе 100 лет!
+
+// // let flag = 0;
+// // alert(Boolean(flag));
+// // alert(String(flag));
+
+// alert( 2 + (2 + "2"))
+
+// alert( 2 + 2 + "1")
